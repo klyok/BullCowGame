a405533bc421cb83dec54721b8f914e1b3becd47
Klyok <yehekim@gmail.com>
2019-01-05 18:59:20 +0300

    Planned methods for changing MyMaxTries, counting bulls & cows and increasing try.

--- FBullCowGame.h
+++ FBullCowGame.h
@@ -12,11 +12,14 @@ public:
     void Reset(); // TODO make a more rich return value
     bool CheckGuessValidity(std::string); // TODO make a more rich return value
 
+    // TODO provide methods for changing MyMaxTries
+
 // ^^ please try and ignore this and focus on the interface above ^^
 private:
     int MyCurrentTry; // see constructor for initialization
     int MyMaxTries;
     bool IsIsogram(std::string);
+    // provide a method for counting bulls and cows and increasing try
 
 };
 
