2c7ab88c71f2987fd319b84e49a1c1d19bba8368
Klyok <yehekim@gmail.com>
2019-01-06 10:58:23 +0300

    Changed method order for better readability.

--- FBullCowGame.h
+++ FBullCowGame.h
@@ -17,8 +17,9 @@ public:
 
     int32 GetMaxTries() const;
     int32 GetCurrentTry() const;
-    bool IsGameWon() const;
     int32 GetHiddenWordLength() const;
+
+    bool IsGameWon() const;
     bool CheckGuessValidity(FString) const; // TODO make a more rich return value
 
     void Reset(); // TODO make a more rich return value
